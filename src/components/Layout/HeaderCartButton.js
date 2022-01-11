26f65a21989ea6a1332633ba19db3be76afad730
Yashar Akish <yashar_akish@yahoo.com>
2022-01-10 19:40:53 -0500

    created CartButton component with style

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,17 @@
+
+
+import classes from './HeaderCartButton.module.css';
+import CartIcon from '../Cart/CartIcon';
+
+const HeaderCaretButton = props => {
+
+  return(
+    <button className={classes.button}>
+      <span className={classes.icon}><CartIcon /></span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3</span>
+    </button>
+  )
+}
+
+export default HeaderCaretButton;
