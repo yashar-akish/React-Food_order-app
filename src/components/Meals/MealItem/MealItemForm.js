c79458667fa2759376a76dbf04cfe092ac39521e
Yashar Akish <yashar_akish@yahoo.com>
2022-01-13 21:46:18 -0500

    used uuseRef and useContext to handel UseReduser for adding the item with related data to the cart

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,3 +1,5 @@
+import { useRef, useState } from 'react';
+
 
 import Input from '../../UI/Input';
 
@@ -5,9 +7,30 @@ import classes from './MealItemForm.module.css';
 
 const MealItemForm = props => {
 
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
+  const amountInputRef = useRef();
+
+  const submitHandler = event => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount; // because current value always return string, by adding + sign it get convert to number  
+
+    if(enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5){
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  }
+
   return(
-    <form className={classes.form}>
-      <Input label='Amount' input={{
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input
+        ref={amountInputRef}
+        label='Amount' 
+        input={{
         id: 'amount' + props.id,
         type: 'number',
         min: '1',
@@ -16,6 +39,7 @@ const MealItemForm = props => {
         defaultValue: '1',
       }}/>
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount <b>between 1 to 5</b></p>}
     </form>
   )
 }
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,6 +9,15 @@ const defaultCartState = {
 };
 const CartReducer = (state, action) =>{
 
+  if (action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
+  }
   return defaultCartState;
 }
 
