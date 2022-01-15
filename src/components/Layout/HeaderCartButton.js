d20fdda61f3572af27aa4a74f8cdb71f76dc4c4b
Yashar Akish <yashar_akish@yahoo.com>
2022-01-15 14:09:35 -0500

    created a function to animate the cart button anytime any new item get added to cart by using useEffect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,5 +1,5 @@
 
-import {useContext} from 'react';
+import {useContext, useEffect, useState} from 'react';
 
 import CartContext from '../../store/cart-context';
 
@@ -8,13 +8,34 @@ import CartIcon from '../Cart/CartIcon';
 
 const HeaderCaretButton = props => {
 
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false)
+
   const cartCtx = useContext(CartContext);
-  const numberOfCartItem = cartCtx.items.reduce((currentNumber, item) => {
+
+  const {items} = cartCtx;
+  const numberOfCartItem = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items])
+
   return(
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}><CartIcon /></span>
       <span>Your Cart</span>
       <span className={classes.badge}>{numberOfCartItem}</span>
