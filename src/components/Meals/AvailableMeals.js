a0a45db68c14bb8a3a3cb48dba59fbe74e34ebb0
Yashar Akish <yashar_akish@yahoo.com>
2022-01-22 12:59:12 -0500

    handled loading time and http error

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -9,11 +9,16 @@ import classes from "./AvailableMeals.module.css";
 const AvailableMeals = () => {
 
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(()=>{
     const fetchMeals = async () => {
 
       const response = await fetch('https://react-http-4c059-default-rtdb.firebaseio.com/meals.json');
+      if(!response.ok){
+        throw new Error('Sorry something went wrong!!');
+      }
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -27,12 +32,29 @@ const AvailableMeals = () => {
         })
       }
 
-      setMeals(loadedMeals)
+      setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+    setHttpError(error.message);
+    });
+
   }, []);
 
+
+  if(isLoading) {
+    return (
+      <section className={classes.MealsLoading}><p>Loading...</p></section>
+    );
+  }
+
+  if(httpError) {
+    return (
+      <section className={classes.MealsError} ><p>{httpError}</p></section>
+    )
+  }
 
   return (
     <section className={classes.meals}>
